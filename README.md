91495d052e1cf4f7e0d6d508c6ba068a85da68f1
Josué Gimenes <josuegalvares@gmail.com>
2020-01-17 19:02:11 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+<h1 align="center">
+    <img alt="GoStack" src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/bootcamp-header.png" width="200px" />
+</h1>
+
+<p align="center">
+  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/josuegimenes/rocketseat-bootcamp-gostack-gobarber-api-node-security-module16?color=%2304D361">
+
+  <a href="https://rocketseat.com.br">
+    <img alt="Made by Rocketseat" src="https://img.shields.io/badge/made%20by-Rocketseat-%2304D361">
+  </a>
+
+  <img alt="License" src="https://img.shields.io/badge/license-MIT-%2304D361">
+</p>
+
+# Módulo 16: Aplicação Backend – GoBarber (Segurança no Node.js)
+
+Sistema de barbearia para agendamento de serviços.
---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,70 @@
+import User from '../models/User';
+import File from '../models/File';
+
+import Cache from '../../lib/Cache';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists.' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    // Clear cache when new provider registration appears.
+    // Apagar cache quando surgir novo cadastro de provider.
+    if (provider) {
+      await Cache.invalidate('providers');
+    }
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match.' });
+    }
+
+    await user.update(req.body);
+
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
+
+    return res.json({
+      id,
+      name,
+      email,
+      avatar,
+    });
+  }
+}
+
+export default new UserController();
